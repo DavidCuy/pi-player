e050241bee608539c926ab936bf37ba2e961f6af
David Cuy <david.cuy.sanchez@gmail.com>
2022-06-12 18:41:18 -0500

    feature: router de modelos web

--- video-admin/portal/routes/ScheduleRouter.py
+++ video-admin/portal/routes/ScheduleRouter.py
@@ -5,8 +5,8 @@ from portal.app.Services import ScheduleService
 schedule_router = Blueprint('schedule', __name__)
 schedule_service = ScheduleService()
 
-schedule_router.route('/', methods=['GET'], defaults={'service': schedule_router}) (index)
-schedule_router.route('/', methods=['POST'], defaults={'service': schedule_router}) (store)
-schedule_router.route('/<id>', methods=['GET'], defaults={'service': schedule_router}) (find)
-schedule_router.route('/<id>', methods=['PUT'], defaults={'service': schedule_router}) (update)
-schedule_router.route('/<id>', methods=['DELETE'], defaults={'service': schedule_router}) (delete)
+schedule_router.route('/', methods=['GET'], defaults={'service': schedule_service}) (index)
+schedule_router.route('/', methods=['POST'], defaults={'service': schedule_service}) (store)
+schedule_router.route('/<id>', methods=['GET'], defaults={'service': schedule_service}) (find)
+schedule_router.route('/<id>', methods=['PUT'], defaults={'service': schedule_service}) (update)
+schedule_router.route('/<id>', methods=['DELETE'], defaults={'service': schedule_service}) (delete)
