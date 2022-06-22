105c559004fe98b30411a11896c97542c3ceec10
David Cuy <david.cuy.sanchez@gmail.com>
2022-06-21 23:44:43 -0500

    feature: se integra endpoint para reproduccion de videos

--- web-player/player/scripts.js
+++ web-player/player/scripts.js
@@ -1,8 +1,25 @@
 const videoPlayer = document.getElementById("pivideo")
+let video_order = 0
+let video_source = null
+
+const formatDatetime = (date) => {
+    return `${date.getFullYear().toString().padStart(4, '0')}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')} ${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}`
+}
+
+const update_url = () => {
+    const qParams = new URLSearchParams({
+        order: video_order,
+    })
+    let newurl = `${window.location.protocol}//${window.location.host}${window.location.pathname}?${qParams}`
+    window.history.pushState({ path: newurl }, '', newurl)
+}
 
 const fetchVideo = () => {
-    const cur_datetime = new Date()
-    const url = `http://localhost:5000/static/order_files/1.json`
+    const qParams = new URLSearchParams({
+        current_datetime: formatDatetime(new Date()),
+        current_order: video_order,
+    })
+    const url = `http://localhost:5000/schedule/current_video?${qParams}`
     fetch(url, {
             method: 'GET',
             headers: {
@@ -10,21 +27,33 @@ const fetchVideo = () => {
             }
         })
         .then((response) => response.json())
-        .then((json) => console.log(json))
+        .then((dataVideo) => {
+            update_url()
+            video_order = dataVideo.order
+            video_source = `http://localhost:5000/static/${dataVideo.video_file}`
+
+            videoPlayer.innerHTML = ''
+            const source = document.createElement('source')
+            source.setAttribute('src', video_source)
+            source.setAttribute('type', dataVideo.format)
+
+            videoPlayer.appendChild(source)
+            videoPlayer.load()
+            videoPlayer.play()
+        })
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    const url = `http://localhost:5000/static/order_files/1.json`
-    fetch(url, {
-            method: 'GET',
-            headers: {
-                'accepts': 'application/json'
-            }
-        })
-        .then((response) => response.json())
-        .then((json) => console.log(json))
+    const params = new URLSearchParams(window.location.search)
+    const order_query = params.get('order')
+    if (order_query == null) {
+        update_url()
+    } else {
+        video_order = parseInt(order_query)
+    }
+    fetchVideo()
 })
 
 videoPlayer.onended = () => {
-    alert("The video has ended");
+    fetchVideo()
 };
